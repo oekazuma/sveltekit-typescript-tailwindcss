6143244beafc2614f94a1678d014c53fbc52c541
Kazuma Oe <koneko0604@gmail.com>
2022-06-06 02:37:34 +0000

    update

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,18 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+const production = process.env.NODE_ENV === 'production';
 
 const config = {
-	plugins: [tailwindcss(), autoprefixer]
+	plugins: [
+		tailwindcss(),
+		autoprefixer,
+		production &&
+			cssnano({
+				preset: 'default'
+			})
+	]
 };
 
 module.exports = config;
